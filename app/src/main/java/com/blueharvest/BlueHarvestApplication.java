b13d0537076764ff7e43c94b1748cbc8794de715
Lee Abraham <richard.lee.abraham@gmail.com>
2021-04-20 12:41:05 -0500

    Initialized application

---
+++ app/src/main/java/com/blueharvest/BlueHarvestApplication.java
@@ -0,0 +1,11 @@
+package com.blueharvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueHarvestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BlueHarvestApplication.class, args);
+	}
+}
---
+++ app/src/test/java/com/blueharvest/BlueHarvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueharvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueHarvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
